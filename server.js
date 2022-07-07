4c10e419021c3fcb6de3f7d49136a6782a03b9fa
AshifAli007 <theashifkhan@gmail.com>
2022-07-07 16:05:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images');
+    },
+    filename:(req, file, cb)=>{
+        cb(null, file.originalname)
+    }
+
+})
+
+const upload = multer({storage: fileStorageEngine});
+
+
+
+app.get('/status', (req, res)=>{
+    res.send('ok');
+});
+
+app.post('/single', upload.single('image'),(req, res)=>{
+    console.log(req.file);
+    res.send("single File upload success");
+})
+
+app.listen(4000, ()=>{
+    console.log('server listening on port 4000');
+});
